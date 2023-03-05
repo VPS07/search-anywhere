fc1c8a5a594da5f0809824bab4c3af57fb3c6a54
vps07 <vikysingh07860@gmail.com>
2023-03-05 22:11:26 +0530

    removed background.js

--- content-script.js
+++ content-script.js
@@ -7,52 +7,53 @@ fetch(browser.runtime.getURL("/search.html"))
     div.classList.add("search__container");
     document.body.appendChild(div);
 
-    browser.runtime.onMessage.addListener(function (
-      message,
-      sender,
-      sendResponse
-    ) {
-      // console.log(message.message);
+    const pageURL = document.location.href;
+    const newUrl = new URL(pageURL);
+    const params = new URLSearchParams(newUrl.search);
+    const query = params.get("q");
 
-      const pageURL = message.message;
-      const url = new URL(pageURL);
-      const params = new URLSearchParams(url.search);
-      const query = params.get("q");
+    // getting all links id
+    const duckduckgoSearch = document.getElementById("duckduckgo__search");
+    const bingSearch = document.getElementById("bing__search");
+    const googleSearch = document.getElementById("google__search");
+    const braveSearch = document.getElementById("brave__search");
+    const chatgptSearch = document.getElementById("chatgpt__search");
 
-      // getting all links id
-      const duckduckgoSearch = document.getElementById("duckduckgo__search");
-      const bingSearch = document.getElementById("bing__search");
-      const googleSearch = document.getElementById("google__search");
-      const braveSearch = document.getElementById("brave__search");
-      const chatgptSearch = document.getElementById("chatgpt__search");
-
-      const newUrlDuckduckgo =
-        "https://www.duckduckgo.com/?q=" + encodeURIComponent(query);
-      const newUrlBing =
-        "https://www.bing.com/search?q=" + encodeURIComponent(query);
-      const newUrlGoogle =
-        "https://www.google.com/search?q=" + encodeURIComponent(query);
-      const newUrlBrave =
-        "https://search.brave.com/search?q=" + encodeURIComponent(query);
-
-      if (query) {
-        duckduckgoSearch.href = newUrlDuckduckgo;
-        bingSearch.href = newUrlBing;
-        googleSearch.href = newUrlGoogle;
-        braveSearch.href = newUrlBrave;
-
-        chatgptSearch.onclick = function cpoyTxt() {
-          navigator.clipboard
-            .writeText(query)
-            .then(() => {
-              console.log("Text copied to clipboard");
-            })
-            .catch((error) => {
-              alert("Failed to copy text: ", error);
-            });
-        };
-        chatgptSearch.href = "https://chat.openai.com/";
-      }
-      // console.log(bingSearch); // "search query"
+    fetch(browser.runtime.getURL("/images/")).then((res) => {
+      duckduckgoSearch.querySelector("img").src = res.url + "duckduckgo.png";
+      bingSearch.querySelector("img").src = res.url + "bing.png";
+      googleSearch.querySelector("img").src = res.url + "google.png";
+      braveSearch.querySelector("img").src = res.url + "brave.png";
+      chatgptSearch.querySelector("img").src = res.url + "chatgpt.png";
     });
+
+    const newUrlDuckduckgo =
+      "https://www.duckduckgo.com/?q=" + encodeURIComponent(query);
+    const newUrlBing =
+      "https://www.bing.com/search?q=" + encodeURIComponent(query);
+    const newUrlGoogle =
+      "https://www.google.com/search?q=" + encodeURIComponent(query);
+    const newUrlBrave =
+      "https://search.brave.com/search?q=" + encodeURIComponent(query);
+
+    if (query) {
+      duckduckgoSearch.href = newUrlDuckduckgo;
+      bingSearch.href = newUrlBing;
+      googleSearch.href = newUrlGoogle;
+      braveSearch.href = newUrlBrave;
+
+      chatgptSearch.onclick = function cpoyTxt() {
+        navigator.clipboard
+          .writeText(query)
+          .then(() => {
+            console.log("Text copied to clipboard");
+          })
+          .catch((error) => {
+            alert("Failed to copy text: ", error);
+          });
+      };
+      chatgptSearch.href = "https://chat.openai.com/";
+    }
+
+    console.log(document.location.href);
   });
